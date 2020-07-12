e83e813381b7565aefac5847ae3ce0ca5dd4ff3f
jithish <63664628+jithish-sekar@users.noreply.github.com>
2020-07-12 15:50:51 +0530

    router

--- src/components/router_component/router.js
+++ src/components/router_component/router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { Switch, Route, withRouter } from 'react-router-dom';
 import LandingPageComponent from '../landingpage_component/landingpage';
 
 const Router = () => (
